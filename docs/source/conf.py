b9cc4a893d44f808f4f7c39d30dccd88156aaa03
Stefano Kowalke <info@arroba-it.de>
2023-07-17 13:29:40 +0200

    Fix the build process

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Commons Cargobike Handbook'
-copyright = '2023, Verband Freier Lastenräder'
-author = 'Hannes'
+project = 'Lumache'
+copyright = '2021, Graziella'
+author = 'Graziella'
 
 release = '0.1'
 version = '0.1.0'
