dc7d39d1dde4d90f5f9417a7c35a50aecaa4feb8
Stefano Kowalke <info@arroba-it.de>
2023-07-17 14:18:29 +0200

    Change author

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2023, Verband freier Lastenräder'
+author = 'Verband freier Lastenräder'
 
 release = '0.1'
 version = '0.1.0'
